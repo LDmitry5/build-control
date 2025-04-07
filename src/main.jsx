5ec2c2487bfcea406eec69cd2933939c9924031a
Dmitry Likhachev <kolt_1991@mail.ru>
2025-04-07 14:12:50 +0300

    build

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/build-control">
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>
